734325ad92dbb04cf5f6148b74c5d18f1ea0a01a
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-05-10 16:20:07 -0400

    Header and SubHeading complete

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Header.css";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new flavour" />
+        <h1 className="app__header-h1">The Key To Fine Dining</h1>
+        <p className="p__opensans" style={{ margin: "2rem 0" }}>
+          Donec adipiscing tristique risus nec feugiat in fermentum. Feugiat sed
+          lectus vestibulum mattis ullamcorper. Lectus proin nibh nisl
+          condimentum id.
+        </p>
+        <button type="button" className="custom__button">
+          Explore Menu
+        </button>
+      </div>
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header_img" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
