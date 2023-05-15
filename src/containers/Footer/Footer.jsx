67514bd66afb411a1f3d4474d257195c41b3ea70
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-05-15 17:35:01 -0400

    Newsletter component complete

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Footer.css";
+import { images } from "../../constants";
+import { FooterOverlay, Newsletter } from "../../components";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="app__footer section__padding">
+      <FooterOverlay />
+      <Newsletter />
+    </div>
+  );
 };
 
 export default Footer;
